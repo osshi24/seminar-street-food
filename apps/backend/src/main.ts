b85836c8c8d15be6ed6e9ebf9ccbd98b2f8bff65
TrungHung07 <140092572+TrungHung07@users.noreply.github.com>
2026-03-11 12:58:58 +0700

    Add email notification templates

---
+++ apps/backend/src/main.ts
@@ -0,0 +1,46 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as cookieParser from 'cookie-parser';
+import helmet from 'helmet';
+import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { TransformInterceptor } from './common/interceptors/transform.interceptor';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT', 3001);
+
+  app.setGlobalPrefix('api');
+
+  app.use(cookieParser());
+
+  app.use(
+    helmet({
+      contentSecurityPolicy: false,
+    }),
+  );
+
+  app.enableCors({
+    origin: configService.get<string>('FRONTEND_URL', 'http://localhost:3000'),
+    credentials: true,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new TransformInterceptor());
+
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}/api`);
+}
+
+bootstrap();
